b5ee80b0b3430a25ef24b13fd08c0ab110470144
saufiroja <saufiroja08@gmail.com>
2021-12-01 16:43:44 +0800

    generete nest auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { typeOrmConfig } from './config/typeorm.config';
 import { FeedModule } from './feed/feed.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), FeedModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), FeedModule, AuthModule],
 })
 export class AppModule {}
