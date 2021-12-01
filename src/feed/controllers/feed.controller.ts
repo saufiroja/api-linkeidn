117424fa33d70370c4ba117600b5112f973f51ce
saufiroja <saufiroja08@gmail.com>
2021-12-01 15:22:00 +0800

    build fitur get all and get by id

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { createFeedDto } from '../dto/create.feed.dto';
 import { Feed } from '../models/feed.entity';
 import { FeedService } from '../service/feed.service';
@@ -7,7 +7,19 @@ import { FeedService } from '../service/feed.service';
 export class FeedController {
   constructor(private feedService: FeedService) {}
 
-  //   create post
+  // Get All Posts
+  @Get()
+  getAllPosts(feed: Feed): Promise<Feed[]> {
+    return this.feedService.getAllPosts(feed);
+  }
+
+  // Gett Post By Id
+  @Get(':id')
+  getPostById(@Param('id') id: string): Promise<Feed> {
+    return this.feedService.getPostById(id);
+  }
+
+  // create post
   @Post()
   createPost(@Body() createfeedDto: createFeedDto): Promise<Feed> {
     return this.feedService.createPost(createfeedDto);
--- src/feed/service/feed.service.ts
+++ src/feed/service/feed.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { createFeedDto } from '../dto/create.feed.dto';
 import { Feed } from '../models/feed.entity';
@@ -11,6 +11,22 @@ export class FeedService {
     @InjectRepository(FeedRepository)
     private readonly feedRepository: FeedRepository,
   ) {}
+
+  // get all posts
+  async getAllPosts(feed: Feed): Promise<Feed[]> {
+    return await this.feedRepository.find(feed);
+  }
+
+  // get post by id
+  async getPostById(id: string): Promise<Feed> {
+    const postById = await this.feedRepository.findOne({ id });
+
+    if (!postById) {
+      throw new NotFoundException(`Feed with ID "${id}" not found`);
+    }
+
+    return postById;
+  }
 
   // create feed
   createPost(createFeedDto: createFeedDto): Promise<Feed> {
